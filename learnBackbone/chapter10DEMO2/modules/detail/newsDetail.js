e65541e48ca2a98f27565564334da29dfda2d11e
DanielLam <lwd931227@126.com>
2015-12-22 23:52:50 +0800

    backbone实战实例2完善

--- learnBackbone/chapter10DEMO2/modules/detail/newsDetail.js
+++ learnBackbone/chapter10DEMO2/modules/detail/newsDetail.js
@@ -1,14 +1,12 @@
-define(['jquery','underscore','backbone','text!modules/detail/newsDetailView.html'], function ($,_,Backbone,detailViewTemplate){
-    var detailView=Backbone.View.extend({
-        template:_.template(detailViewTemplate),
-        initialize:function(){
-            this.model.bind('GetDetail:Detail'),
-            this.render,
-            this
+define(['jquery', 'underscore', 'backbone', 'text!modules/detail/newsDetailView.html'], function ($, _, Backbone, detailViewTemplate) {
+    var detailView = Backbone.View.extend({
+        template: _.template(detailViewTemplate),
+        initialize: function () {
+            this.model.bind('GetDetail:Detail', this.render, this);
         },
-        render:function(){
+        render: function () {
             $(this.el).append(this.template(this.model.toJSON()));
-            this.trigger('renderDetail:Detail',this,'');
+            this.trigger('renderDetail:Detail', this, '');
             return this;
         }
     });
