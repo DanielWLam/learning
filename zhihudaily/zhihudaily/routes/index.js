6752de671e968b21799a27f2588809fe801b3a67
DanielWLam <lwd931227@126.com>
2016-05-23 23:38:21 +0800

    daily modify

--- zhihudaily/zhihudaily/routes/index.js
+++ zhihudaily/zhihudaily/routes/index.js
@@ -5,45 +5,40 @@ var cheerio = require('cheerio');
 var async = require('async');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  async.series([
-      function(cb){
-        var result = [];
-        request('http://daily.zhihu.com/',function(err,res,body){
-          if(err){
-            return;
-          }
-
-          $ = cheerio.load(body);
-
-          var boxes = $('.box');
-          var imgTag = boxes.find('img[src]');
-          var imgs = [];
-          var titleTag = boxes.find('.title');
-          var titles = [];
-          var aTag = boxes.find('.link-button');
-          var links = [];
-
-          imgTag.each(function(i,item){
-            imgs.push($(this).attr('src'));
-          });
-
-          aTag.each(function(i,item){
-            links.push($(this).attr('href'));
-          });
-
-          titleTag.each(function(i,item){
-            titles.push($(this).text());
-          });
-          for(var i=0,len=imgs.length;i<len;i++){
-            result.push({title:titles[i],img:imgs[i],link:links[i]});
-          }
-          callback(null,result);
-        });
+router.get('/', function(req, response, next) {
+    var result = [];
+    request('http://daily.zhihu.com/',function(err,res,body){
+      if(err){
+        return;
       }
-  ],function(err,result){
-    res.render('index', { title: 'Express' ,result:result});
-  });
+
+      $ = cheerio.load(body);
+
+      var boxes = $('.box');
+      var imgTag = boxes.find('img[src]');
+      var imgs = [];
+      var titleTag = boxes.find('.title');
+      var titles = [];
+      var aTag = boxes.find('.link-button');
+      var links = [];
+
+      imgTag.each(function(i,item){
+        imgs.push($(this).attr('src'));
+      });
+
+      aTag.each(function(i,item){
+        links.push($(this).attr('href'));
+      });
+
+      titleTag.each(function(i,item){
+        titles.push($(this).text());
+      });
+      for(var i=0,len=imgs.length;i<len;i++){
+        result.push({title:titles[i],img:imgs[i],link:links[i]});
+      }
+      response.render('index', { title: 'Express' ,result:result});
+    });
+    
 });
 
 module.exports = router;
