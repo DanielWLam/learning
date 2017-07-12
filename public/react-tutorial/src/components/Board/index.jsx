dca166e0917203f7ed1139d1643cba4c2d894bff
DanielWLam <lwd931227@126.com>
2017-07-12 20:40:38 +0800

    react tutorial无法启动

---
+++ public/react-tutorial/src/components/Board/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Square from '../Square';
+export default class Board extends Component {
+  render () {
+    return (
+      <Square value="123"></Square>
+    )
+  }
+}
