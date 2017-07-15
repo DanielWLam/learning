59148b7cad654ac2381517a4457160ce52398602
DanielWLam <lwd931227@126.com>
2017-07-15 14:07:55 +0800

    init

---
+++ public/github-words/src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'github-words'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
